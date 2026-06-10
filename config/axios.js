1b59b54923b0a5943f77fb50f4283f68ab80db03
solomon sheshinbwa dickson <solomodickson737@gmail.com>
2026-06-10 09:21:15 +0100

    added correction in the config

--- config/axios.js
+++ config/axios.js
@@ -1,9 +1,6 @@
-
 import axios from "axios"
-import {VITE_BASEURL} from "./env.js";
 
 const api = axios.create({
-    baseURL:VITE_BASEURL,
     withCredentials: true
 })
 
--- config/env.js
+++
@@ -1,1 +0,0 @@
-export const {VITE_BASEURL} = import.meta.env
